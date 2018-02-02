915fcf3292e5b09c534a86c8895a2f03f5dc70ff
lschmittling <lanceschmittling@Lances-MBP>
2018-02-02 13:33:57 -0600

    Final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,28 @@
-// Computer picks a random letter
-// User enters guess through keyboard letter guesses
-// User gets 10 guesses
-// If correct, game is over and resets
-// If incorrect, number of guesses goes down
-// If incorrect, guessed letter get placed in a wrong div
-// Once number of guesses hits 0, game resets
+// VARIABLES
 
 // Array that holds the alphabet
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Variables that track wins, losses and guesses
+// Number of times user has guessed the letter correctly
 var wins = 0;
+
+// Number of times user has guessed incorrectly (10 guesses) and lost the game
 var losses = 0;
+
+// Number of guesses remaining
 var guessesLeft = 10;
 
-// Variable for the users guesses so far
+// Holds the specific letters that the user typed
 var guessesSoFar = [];
 
-// Variable for the users key press
+// Holds users guess (single letter)
 var userGuess = [];
 
-// Computer randomly picks a letter from the alphabet array
+// Computer randomly picks a letter from array above
 var compChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-// On Key Up function
+// GAME LOOP
+
 document.onkeyup = function (event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
@@ -31,14 +30,12 @@ document.onkeyup = function (event) {
         guessesSoFar[guessesSoFar.length] = userGuess;
         guessesLeft--;
     }
-
     if (compChoice == userGuess) {
         wins++;
         guessesLeft = 10;
         guessesSoFar = [];
         compChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
     }
-
     if (guessesLeft == 0) {
         losses++;
         guessesLeft = 10;
@@ -46,16 +43,10 @@ document.onkeyup = function (event) {
         compChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
     }
 
-    var html =
-        "<p>You chose: " + userGuess + "</p>" +
-        "<p>Remaining Guesses: " + guessesLeft + "</p>" +
-        "<p>wins: " + wins + "</p>" +
-        "<p>losses: " + losses + "</p>";
+// UPDATES HTML WITH VALUES FROM ABOVE
 
-    // Set the inner HTML contents of the #game div to our html string
-    document.querySelector("#game").innerHTML = html;
-};
-
-
-    // console.log("Wins: " + wins + " Losses: " + losses + " Remaining Guesses: " + guessesLeft + " Guessed So Far: " + guessesSoFar + " Computer Picked: " + compChoice);
-
+    document.querySelector("#wins").innerHTML = "" + wins;
+    document.querySelector("#losses").innerHTML = "" + losses;
+    document.querySelector("#guessesLeft").innerHTML = "" + guessesLeft;
+    document.querySelector("#currentGuesses").innerHTML = guessesSoFar;
+}
